c94b9166cda2652fdf36bf66f54eda5975323a6a
Minh Truong <lmtruong27@gmail.com>
2019-11-10 15:00:58 +0700

    qqqq

--- QVNTRUYEN/app/src/main/java/com/example/qvntruyen/FragmentHome.java
+++ QVNTRUYEN/app/src/main/java/com/example/qvntruyen/FragmentHome.java
@@ -1,15 +1,20 @@
 package com.example.qvntruyen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.FrameLayout;
 import android.widget.GridView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +24,8 @@ public class FragmentHome extends Fragment {
     ArrayList<DS_Truyen> img_detail = new ArrayList<DS_Truyen>();
     GridView gView;
     Ad_Truyen listtruyen;
+    FrameLayout frameLayout;
+    //FragmentManager fm = getFragmentManager();
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,6 +34,7 @@ public class FragmentHome extends Fragment {
         gView = (GridView) view.findViewById(R.id.fragment_home_gv_item);
         listtruyen = new Ad_Truyen(getContext(), img_detail);
         gView.setAdapter(listtruyen);
+        Info();
         return view;
     }
 
@@ -41,5 +49,15 @@ public class FragmentHome extends Fragment {
         return list;
     }
 
+    private void Info(){
+        gView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                FragmentTransaction ff = getActivity().getSupportFragmentManager().beginTransaction();
+                ff.replace(R.id.fragment_container, new Item_click());
+                ff.commit();
+            }
+        });
 
+    }
 }
---
+++ QVNTRUYEN/app/src/main/java/com/example/qvntruyen/Item_click.java
@@ -0,0 +1,18 @@
+package com.example.qvntruyen;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Item_click extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_item_click,container,false);
+    }
+}
